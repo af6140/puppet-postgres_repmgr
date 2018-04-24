256744d22b77a58eefacd16264ceafff779b0492
Wang, Dawei <dwang@entertainment.com>
2018-04-24 18:00:14 -0400

    command param

--- manifests/nodes/register_primary.pp
+++ manifests/nodes/register_primary.pp
@@ -1,6 +1,6 @@
 class postgres_repmgr::nodes::register_primary{
     $check_cluster_status = "${::postgres_repmgr::pg_bindir}/repmgr -f ${::postgres_repmgr::conf_dir}/repmgr.conf cluster show 2>/dev/null"
-    $register_cmd = "${::postgres_repmgr::pg_bindir}/repmgr -f ${::postgres_repmgr::conf_dir}/repmgr.conf pirmary register"
+    $register_cmd = "${::postgres_repmgr::pg_bindir}/repmgr -f ${::postgres_repmgr::conf_dir}/repmgr.conf primary register"
     notify {"register_primary_cmd":
       message => "Using register_cmd: ${register_cmd}"
     }
