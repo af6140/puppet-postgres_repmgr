92ac327b3c61f5596aed597d6ca120c1ca5b30e6
Wang, Dawei <dwang@entertainment.com>
2018-05-07 12:58:55 -0400

    prep

--- manifests/nodes/clone_primary.pp
+++ manifests/nodes/clone_primary.pp
@@ -1,5 +1,10 @@
 class postgres_repmgr::nodes::clone_primary{
+
+     ## To Do: Fix logic when to register
+     # DO NOT use for now
+
     $check_cluster_status = "${::postgres_repmgr::conf_dir}/repmgr.conf cluster show 2>/dev/null"
+    $standby_clone_command = "/etc/repmgr/${::postgres_repmgr::pg_version}/standby_clone.sh"
     exec { 'stop_postgresql':
       user    => 'postgres',
       command => $postgres_repmgr::service_stop_cmd,
@@ -9,5 +14,7 @@ class postgres_repmgr::nodes::clone_primary{
       path => "${::postgres_repmgr::pg_bindir}/bin:/bin:/sbin:/usr/bin:/usr/sbin",
       command => "repmgr -f ${::postgres_repmgr::conf_dir}/repmgr.conf -c -R repmgr standby clone",
       user    => 'postgres',
+      environment => [],
+      unless => "systemctl status postgres-${::postgres_repmgr::pg_version}"
     }
 }
--- manifests/nodes/register_standby.pp
+++ manifests/nodes/register_standby.pp
@@ -1,9 +1,18 @@
 class postgres_repmgr::nodes::register_standby{
-    $check_cluster_status = "${::postgres_repmgr::conf_dir}/repmgr.conf cluster show 2>/dev/null"
+
+    ## To Do: Fix logic when to register
+    # Do not use for now.
+    #$check_cluster_status = "${::postgres_repmgr::pg_bindir}/repmgr -f ${::postgres_repmgr::conf_dir}/repmgr.conf cluster show 2>/dev/null"
+
+    $register_cmd = "${::postgres_repmgr::pg_bindir}/repmgr -f ${::postgres_repmgr::conf_dir}/repmgr.conf standby register"
+    notify {"register_standby_cmd":
+      message => "Registering standby using register_cmd: ${register_cmd}"
+    }
     exec { 'register_standby':
-      path => "${::postgres_repmgr::pg_bindir}/bin:/bin:/sbin:/usr/bin:/usr/sbin",
-      command => "repmgr -f ${::postgres_repmgr::conf_dir}/repmgr.conf standby register",
+      command => $register_standby_cmd,
       user    => 'postgres',
+      tries => 5,
+      try_sleep => 5,
       unless => $check_cluster_status, #only execute when no cluster status
     }
 }
