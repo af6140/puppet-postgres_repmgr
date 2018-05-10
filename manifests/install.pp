5b9ff61a4789cb9b29fea583b27528148f761452
Wang, Dawei <dwang@entertainment.com>
2018-05-10 15:19:04 -0400

    set user to postgres

--- manifests/install.pp
+++ manifests/install.pp
@@ -11,6 +11,8 @@ class postgres_repmgr::install {
     replace => 'no', # this is the important property
     content => "From Puppet\n",
     mode    => '4744',
+    owner => 'postgres',
+    group  => 'root',
     require =>
   }
 }
